0775d8f888e1a370aa01e5d8c4a3222e3236bf23
douglas quisbert <duglas3093@gmail.com>
2022-11-28 06:27:23 -0400

    inital commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px
+`
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
+`
+
+const Precio = styled.p`
+    font-size: 24px;
+    span{
+        font-weight: 700;
+    }
+`
+    const Texto = styled.p`
+    font-size: 18px;
+    span{
+        font-weight: 700;
+    }
+`
+
+export const Resultado = ({ resultado }) => {
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
+
+    return (
+        <Contenedor>
+            <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="imagen" />
+            <div>
+                <Precio>El precio es de: <span>{ PRICE }</span></Precio>
+                <Texto>Precio más alto del día: <span>{ HIGHDAY }</span></Texto>
+                <Texto>Precio más bajo del día: <span>{ LOWDAY }</span></Texto>
+                <Texto>Variación últimas 24 hrs: <span>{ CHANGEPCT24HOUR }</span></Texto>
+                <Texto>Última actualización <span>{ LASTUPDATE }</span></Texto>
+            </div>
+        </Contenedor>
+    )
+}
---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'MXN', nombre: 'Pesos mexicanos'},
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'GBP', nombre: 'Libra esterlina'},
+]
+
+export { monedas }
